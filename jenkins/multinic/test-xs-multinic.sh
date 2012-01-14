b83c766e64f00da0134cc892b45c94e7954a5eb7
Ewan Mellor <ewan.mellor@citrix.com>
2012-01-13 18:08:39 -0800

    Revert change to IP address range specification.  The /29 was correct because its --network_size that determines that value, not --fixed_range_v4.

--- jenkins/multinic/test-xs-multinic.sh
+++ jenkins/multinic/test-xs-multinic.sh
@@ -190,9 +190,9 @@ os-vpx-rc "$MULTINIC_USER" "$MULTINIC_PASS" "$MULTINIC_PROJECT"
 . novarc
 
 nova-manage network create --label public-test --fixed_range_v4 10.1.0.0/24 --num_networks=1 --network_size=8 --bridge=$TENANT_BRIDGE
-add_on_exit "nova-manage network delete --fixed_range=10.1.0.0/24"
+add_on_exit "nova-manage network delete --fixed_range=10.1.0.0/29"
 nova-manage network create --label private-test --fixed_range_v4 10.2.0.0/24 --num_networks=1 --network_size=8 --bridge=$TENANT_BRIDGE
-add_on_exit "nova-manage network delete --fixed_range=10.2.0.0/24"
+add_on_exit "nova-manage network delete --fixed_range=10.2.0.0/29"
 
 # Check if mykey is already present
 key=$(euca-describe-keypairs | grep "mykey" | awk {'print $2'} || true)
