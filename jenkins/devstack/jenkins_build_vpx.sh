3f2e95d9cc4cd17d9ee2594ff87f0fb060c7393d
Renuka Apte <renuka.apte@citrix.com>
2012-01-25 13:08:56 -0800

    Run devstack vpx build as root

---
+++ jenkins/devstack/jenkins_build_vpx.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eux
+
+thisdir=$(dirname $(readlink -f "$0"))
+
+. "$thisdir/../common.sh"
+
+server=brontitall.eng.hq.xensource.com
+stackdir="/tmp/stack"
+
+mkdir -p $stackdir
+
+cd $stackdir
+
+if [ ! -d $stackdir/devstack ]
+then
+    git clone git@github.com:renuka-apte/devstack.git
+fi
+
+cd devstack
+git checkout xenservermodif
+
+cd $stackdir/devstack
+if [ ! -f localrc ]
+then
+    wget http://gold.eng.hq.xensource.com/localrc
+fi
+
+cd tools/xen
+./build_xva.sh
+
+mv stage /tmp/
+cd ../../../
+scp -r devstack root@$server:~/
+mv /tmp/stage $stackdir/devstack/tools/xen
+
+remote_execute "root@$server" \
+    "$thisdir/on-host.sh"
--- jenkins/run-devstack.sh
+++ jenkins/run-devstack.sh
@@ -8,36 +8,4 @@ thisdir=$(dirname $(readlink -f "$0"))
 
 enter_jenkins_test
 
-sudo su -
-
-server=brontitall.eng.hq.xensource.com
-stackdir="/tmp/stack"
-
-mkdir -p $stackdir
-
-cd $stackdir
-
-if [ ! -d $stackdir/devstack ]
-then
-    git clone git@github.com:renuka-apte/devstack.git
-fi
-
-cd devstack
-git checkout xenservermodif
-
-cd $stackdir/devstack
-if [ ! -f localrc ]
-then
-    wget http://gold.eng.hq.xensource.com/localrc
-fi
-
-cd tools/xen
-./build_xva.sh
-
-mv stage /tmp/
-cd ../../../
-scp -r devstack root@$server:~/
-mv /tmp/stage $stackdir/devstack/tools/xen
-
-remote_execute "root@$server" \
-                   "$thisdir/devstack/on-host.sh"
+sudo su -c "$thisdir/devstack/jenkins_build_vpx.sh" root
