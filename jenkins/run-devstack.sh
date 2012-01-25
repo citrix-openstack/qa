6ef849c00bc81a21968c02a2f77cb0c140ee3071
Renuka Apte <renuka.apte@citrix.com>
2012-01-25 12:45:31 -0800

    Run devstack vpx build as root

--- jenkins/run-devstack.sh
+++ jenkins/run-devstack.sh
@@ -6,7 +6,9 @@ thisdir=$(dirname $(readlink -f "$0"))
 
 . "$thisdir/common.sh"
 
-#enter_jenkins_test
+enter_jenkins_test
+
+sudo su -
 
 server=brontitall.eng.hq.xensource.com
 stackdir="/tmp/stack"
