86f49ef8e46e09a4c7f4305df0acf4cc70965a66
Renuka Apte <renuka.apte@citrix.com>
2012-01-30 11:10:45 -0800

    Remove wrong git install

--- jenkins/run-devstack-helper.sh
+++ jenkins/run-devstack-helper.sh
@@ -6,38 +6,6 @@ thisdir=$(dirname $(readlink -f "$0"))
 
 . "$thisdir/common.sh"
 
-# Install basics for vi and git
-yum -y  --enablerepo=base install gcc make vim-enhanced zlib-devel openssl-devel
-
-# Simple but usable vimrc
-if [ ! -e /root/.vimrc ]; then
-    cat > /root/.vimrc <<EOF
-syntax on
-se ts=4
-se expandtab
-se shiftwidth=4
-EOF
-fi
-
-# Use the pretty vim
-if [ -e /usr/bin/vim ]; then
-    rm /bin/vi
-    ln -s /usr/bin/vim /bin/vi
-fi
-
-# Install git 
-if ! which git; then
-    DEST=/tmp/
-    GITDIR=$DEST/git-1.7.7
-    cd $DEST
-    rm -rf $GITDIR*
-    wget http://git-core.googlecode.com/files/git-1.7.7.tar.gz
-    tar xfv git-1.7.7.tar.gz
-    cd $GITDIR
-    ./configure
-    make install
-fi
-
 cd $stackdir
 
 cd devstack
