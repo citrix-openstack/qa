c90924d569edc90f30eacbc426e7ede551ae9913
Renuka Apte <renuka.apte@citrix.com>
2012-01-30 10:14:10 -0800

    Fix git bug and correct custom server

--- jenkins/run-devstack-helper.sh
+++ jenkins/run-devstack-helper.sh
@@ -6,8 +6,40 @@ thisdir=$(dirname $(readlink -f "$0"))
 
 . "$thisdir/common.sh"
 
-server=brontitall.eng.hq.xensource.com
+server="${Server-$TEST_XENSERVER}"
 stackdir="/tmp/stack"
+
+# Install basics for vi and git
+yum -y  --enablerepo=base install gcc make vim-enhanced zlib-devel openssl-devel
+
+# Simple but usable vimrc
+if [ ! -e /root/.vimrc ]; then
+    cat > /root/.vimrc <<EOF
+syntax on
+se ts=4
+se expandtab
+se shiftwidth=4
+EOF
+fi
+
+# Use the pretty vim
+if [ -e /usr/bin/vim ]; then
+    rm /bin/vi
+    ln -s /usr/bin/vim /bin/vi
+fi
+
+# Install git 
+if ! which git; then
+    DEST=/tmp/
+    GITDIR=$DEST/git-1.7.7
+    cd $DEST
+    rm -rf $GITDIR*
+    wget http://git-core.googlecode.com/files/git-1.7.7.tar.gz
+    tar xfv git-1.7.7.tar.gz
+    cd $GITDIR
+    ./configure
+    make install
+fi
 
 cd $stackdir
 
