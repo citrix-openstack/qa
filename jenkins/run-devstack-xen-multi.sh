cc6d613f63206bca6c369074b4ce6c11930ec730
John Garbutt <john.garbutt@citrix.com>
2012-05-02 16:52:26 +0100

    Simplify xen-multi test

--- jenkins/run-devstack-xen-multi.sh
+++ jenkins/run-devstack-xen-multi.sh
@@ -10,21 +10,17 @@ thisdir=$(dirname $(readlink -f "$0"))
 # Install first host (master)
 #
 server=$Server1
-localrcURL=$LocalrcUrl
 GUEST_IP=""
 
 . ./run-devstack-xen.sh
 
 #
-# Find IP address of master
-#
-GUEST_NAME=${GUEST_NAME:-"DevStackOSDomU"} # TODO - pull from config
-GUEST_IP=$(ssh_no_hosts "$server" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
-
-#
 # Install second host (compute slave)
 #
 server=$Server2
-localrcURL=$LocalrcUrl2
+
+# Find IP address of master
+GUEST_NAME=${GUEST_NAME:-"DevStackOSDomU"} # TODO - pull from config
+GUEST_IP=$(ssh_no_hosts "$server" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
 
 . ./run-devstack-xen.sh
