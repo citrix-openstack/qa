1deb14aa429d329ef632063c3e92a78c986caa02
John Garbutt <john.garbutt@citrix.com>
2012-05-02 17:19:17 +0100

    More useful to have mutli just install the second host, linked to the first

--- jenkins/run-devstack-xen-multi.sh
+++ jenkins/run-devstack-xen-multi.sh
@@ -7,20 +7,12 @@ thisdir=$(dirname $(readlink -f "$0"))
 . "$thisdir/common-ssh.sh"
 
 #
-# Install first host (master)
-#
-export Server=$Server1
-export GUEST_IP=false
-
-. "$thisdir/run-devstack-xen.sh"
-
-#
 # Install second host (compute slave)
 #
-export Server=$Server2
 
 # Find IP address of master
 export GUEST_NAME=${GUEST_NAME:-"DevStackOSDomU"} # TODO - pull from config
-export GUEST_IP=$(ssh_no_hosts "$server" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
+export GUEST_IP=$(ssh_no_hosts "$Server1" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
 
+export Server=$Server2
 . "$thisdir/run-devstack-xen.sh"
