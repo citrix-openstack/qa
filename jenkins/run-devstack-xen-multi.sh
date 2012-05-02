58540ef1e4f15d1e1938f17561ec3c7ca820d3c7
John Garbutt <john.garbutt@citrix.com>
2012-05-02 17:29:11 +0100

    stop using common-sh in xen-multi

--- jenkins/run-devstack-xen-multi.sh
+++ jenkins/run-devstack-xen-multi.sh
@@ -4,7 +4,6 @@ set -eux
 
 thisdir=$(dirname $(readlink -f "$0"))
 . "$thisdir/common.sh"
-. "$thisdir/common-ssh.sh"
 
 #
 # Install second host (compute slave)
@@ -12,7 +11,7 @@ thisdir=$(dirname $(readlink -f "$0"))
 
 # Find IP address of master
 export GUEST_NAME=${GUEST_NAME:-"DevStackOSDomU"} # TODO - pull from config
-export GUEST_IP=$(ssh_no_hosts "$Server1" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
+export GUEST_IP=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$Server1" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
 
 export Server=$Server2
 . "$thisdir/run-devstack-xen.sh"
