c0f310d82593a76e644a7e43182762c3dd258be3
John Garbutt <john.garbutt@citrix.com>
2012-05-03 17:01:52 +0100

    tidy up the devstack test script

--- jenkins/run-devstack-xen-tests.sh
+++ jenkins/run-devstack-xen-tests.sh
@@ -2,21 +2,16 @@
 
 set -eux
 
-thisdir=$(dirname $(readlink -f "$0"))
-. "$thisdir/common.sh"
-
-#
-# Install second host (compute slave)
-#
-
 # Find IP address of master
 export GUEST_NAME=${GUEST_NAME:-"DevStackOSDomU"} # TODO - pull from config
 export GUEST_IP=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$Server" "xe vm-list --minimal name-label=$GUEST_NAME params=networks | sed -ne 's,^.*3/ip: \([0-9.]*\).*$,\1,p'")
-
 if [ -z "$GUEST_IP" ]
 then
   echo "Failed to find IP address of DevStack DomU on $Server1"
   exit 1
 fi
 
+# Run exercise.sh
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "stack@$GUEST_IP" \ "~/devstack/exercise.sh"
+
+# TODO - run tempest
