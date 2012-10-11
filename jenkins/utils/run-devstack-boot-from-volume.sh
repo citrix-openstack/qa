46cc670d8d50dbb6de96ee61d819835fa2c1547b
Mate Lakat <mate.lakat@citrix.com>
2012-10-11 09:29:11 +0100

    modify run-devstack-boot-from-volume.sh - Script should run on the devstack box

--- jenkins/utils/run-devstack-boot-from-volume.sh
+++ jenkins/utils/run-devstack-boot-from-volume.sh
@@ -1,74 +1,91 @@
 #!/bin/bash
-set -u
-DEVSTACK=$1
-sshpass -p citrix rsync -avz --exclude '.git' --exclude '*.swp' . stack@$DEVSTACK:devstack
-sshpass -p citrix ssh stack@$DEVSTACK << EOF
+set -e 
 set -x
 
-rm -f please_restart_devstack || true
-
-grep -q volume_driver /etc/nova/nova.conf &&
-echo "volume_driver already configured" ||
+##################################################
+### Setup localrc
+##################################################
 (
-cat >> devstack/localrc << EOLOC
+cd devstack
+
+# backup localrc as localrc.original
+[ -e localrc.original ] || cp localrc localrc.original
+
+# create the new localrc
+cp localrc.original localrc
+
+cat >> localrc << EOLOC
+# Activate SM
 EXTRA_OPTS=("agent_version_timeout=1" "volume_driver=nova.volume.xensm.XenSMDriver" "use_local_volumes=False")
-EOLOC
-echo "localrc modified to include SM in nova.conf" >> please_restart_devstack
-)
 
-grep -qe "^disable_service c-api" devstack/localrc &&
-echo "cinder already disabled" ||
-(
-cat >> devstack/localrc << EOLOC
+# Use nova-volume
 disable_service c-api c-sch c-vol cinder
 enable_service n-vol
+
+# Don't reclone - otherwise patches are overwritten
+RECLONE=no
+
+# Exercise specific settings
+# Skip all exercises, except boot from volume
+SKIP_EXERCISES=aggregates,bundle,client-args,client-env,euca,floating_ips,quantum-adv-test,sec_groups,swift,volumes
+# use cirros image
+DEFAULT_IMAGE_NAME=cirros
+# use m1.small (m1.tiny is not enough to launch cirros)
+DEFAULT_INSTANCE_TYPE=m1.small
 EOLOC
-echo "Cinder disabled, using nova-api instead" >> please_restart_devstack
 )
 
-[ -e please_restart_devstack ] &&
+##################################################
+### Apply patches on nova
+##################################################
 (
-    echo "Devstack restart required, because:"
-    cat please_restart_devstack
-
-    sudo service rabbitmq-server status ||
-    sudo service rabbitmq-server start &&
-    echo "Rabbitmq server was not running - started" &&
-    ./run.sh &&
-    rm please_restart_devstack &&
-    echo "Devstack restarted successfully."
+cd nova 
+git checkout -- .
+git checkout master
+git checkout fb101685cc14ed9b0396ce966e571d3fb457c32f
+# Apply John's patch - otherwise n-vol fails to start
+wget -qO - "https://review.openstack.org/gitweb?p=openstack/nova.git;a=commitdiff_plain;h=bb0682fc193833a4ef7c27085ea7c1be31139102" | patch -p1
 )
 
+##################################################
+### Start openstack
+##################################################
+sudo service rabbitmq-server status ||
+sudo service rabbitmq-server start &&
+echo "Workaround - rabbitmq server was not running - now started"
+
+./run.sh
+
+##################################################
+### Activate devstack
+##################################################
 cd devstack
 . openrc admin
-(
-    nova list | grep -q sample_cirros_vm &&
-    echo "cirros image already uploaded"
-) ||
-(
+
+##################################################
+### Upload image to glance
+##################################################
 glance image-create --name cirros \
 --copy-from=http://copper.eng.hq.xensource.com/images/XS-OpenStack/cirros-0.3.0-x86_64-disk.vhd.tgz \
---container-format=ovf --disk-format=vhd &&
-echo "Cirros image uploaded, waiting 10 secs for glance..." &&
-sleep 10 &&
-nova boot --image=cirros --flavor=m1.small sample_cirros_vm &&
-echo "Sample cirros vm launched with small flavor (workaround)"
-)
+--container-format=ovf --disk-format=vhd
+echo "Cirros image uploaded, waiting 10 secs for glance..."
+sleep 10
 
-## RUNNING exercise.sh / boot from volume test
-grep -qe "^SKIP_EXERCISES=aggregates,bundle,cli" localrc ||
-(
-cat >> localrc << EOLOC
-SKIP_EXERCISES=aggregates,bundle,client-args,client-env,euca,floating_ips,quantum-adv-test,sec_groups,swift,volumes
-DEFAULT_IMAGE_NAME=cirros
-EOLOC
-) && echo "localrc modified (only boot from volume will run)"
+##################################################
+### Create the volume using sm
+##################################################
+# Stop nova-volume
+pkill -HUP -f "/opt/stack/nova/bin/nova-volume"
 
-nova-manage sm flavor_list | grep -q gold ||
 nova-manage sm flavor_create gold "Not all that glitters"
+echo -e "y\n" | nova-manage sm backend_add gold nfs name_label=mybackend server=copper serverpath=/bootfromvolume
 
-nova-manage sm backend_list | grep -q copper ||
-echo -e "y\\n" | nova-manage sm backend_add gold nfs name_label=mybackend server=copper serverpath=/bootfromvolume
+# Start nova-volume
+NL=$(echo -e "\n")
+screen -S stack -p n-vol -X stuff "cd /opt/stack/nova && /opt/stack/nova/bin/nova-volume$NL"
+sleep 2
 
-./exercise.sh
-EOF
+##################################################
+### RUNNING exercise.sh
+##################################################
+# ./exercise.sh
