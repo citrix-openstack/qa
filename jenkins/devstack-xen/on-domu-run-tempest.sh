54100bd14615e9e4e09d277fa734ed3903a25a32
John Garbutt <john.garbutt@citrix.com>
2012-12-12 12:06:39 +0000

    try to run tempest tests in the new way

--- jenkins/devstack-xen/on-domu-run-tempest.sh
+++ jenkins/devstack-xen/on-domu-run-tempest.sh
@@ -10,24 +10,7 @@ set -o xtrace
 
 TEMPEST_PARAMS=${1-""}
 
-# get nova branch
-BRANCH=$(cat /opt/stack/nova/.git/HEAD | sed -ne 's,^.*heads/\([a-x0-9/]*\)$,\1,p')
-
-# get tempest from git hub
-rm -rf /opt/stack/tempest
-cd /opt/stack
-git clone https://github.com/openstack/tempest.git
-cd tempest
-git checkout $BRANCH
-
-#
-# Configure tempest
-#
-/opt/stack/devstack/tools/configure_tempest.sh
-
-#
-# HACK: because 11.04 has nose 1.0.0 but we need nose 1.1.2 shipped in 12.04
-sudo pip install -U nose
+cd /opt/stack/tempest
 
 #
 # Run tempest
