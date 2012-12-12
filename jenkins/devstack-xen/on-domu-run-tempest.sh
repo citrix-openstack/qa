2de90d5d7eca66f15ea0c39b25d084dad38407e6
John Garbutt <john.garbutt@citrix.com>
2012-12-12 14:06:51 +0000

    use similar options to the openstack ci when running tempest

--- jenkins/devstack-xen/on-domu-run-tempest.sh
+++ jenkins/devstack-xen/on-domu-run-tempest.sh
@@ -20,6 +20,4 @@ cat ./etc/tempest.conf
 #
 # Run tempest
 #
-# TODO - need a better approach to select tests we skip
-rm -f /opt/stack/tempest/tempest/tests/compute/test_console_output.py
-nosetests $TEMPEST_PARAMS -v tempest -e "test_change_server_password"
+nosetests --with-xunit -sv --nologcapture $TEMPEST_PARAMS tempest
