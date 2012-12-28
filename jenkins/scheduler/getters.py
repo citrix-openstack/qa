6e86a3eba517578ae043f9cde2c4b13117b222bf
Mate Lakat <mate.lakat@citrix.com>
2012-12-28 13:31:50 +0000

    Added lock-get-server-pair

--- jenkins/scheduler/getters.py
+++ jenkins/scheduler/getters.py
@@ -5,15 +5,23 @@ import string
 from selectors import first
 
 
-def wait_for_first(condition):
+def wait_for(selector_factory):
     lock = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
 
     while True:
-        items = database.lock_items('server.database', lock, first(condition))
+        items = database.lock_items('server.database', lock, selector_factory())
         if items:
             break
         time.sleep(1)
 
+    return items, lock
+
+
+def wait_for_first(condition):
+    selector_factory = lambda: first(condition)
+
+    items, lock = wait_for(selector_factory)
+
+    assert len(items) == 1
+
     return items[0], lock
-
-
--- jenkins/scheduler/selectors.py
+++ jenkins/scheduler/selectors.py
@@ -8,3 +8,48 @@ def first(condition=lambda x: True):
         return lambda: False
 
     return term_generator
+
+
+def first_pair_where_vlans_match():
+    """
+    >>> database = []
+    >>> tgen = first_pair_where_vlans_match()
+    >>> callables = [tgen(i) for i in database]
+    >>> [i() for i in callables]
+    []
+    >>> database = [dict(VLAN=1), dict(VLAN=2)]
+    >>> tgen = first_pair_where_vlans_match()
+    >>> callables = [tgen(i) for i in database]
+    >>> [i() for i in callables]
+    [False, False]
+    >>> database = [dict(VLAN=1), dict(VLAN=2), dict(VLAN=2)]
+    >>> tgen = first_pair_where_vlans_match()
+    >>> callables = [tgen(i) for i in database]
+    >>> [i() for i in callables]
+    [False, True, True]
+    >>> database = [dict(VLAN=1)]
+    >>> tgen = first_pair_where_vlans_match()
+    >>> callables = [tgen(i) for i in database]
+    >>> [i() for i in callables]
+    [False]
+    """
+    accumulator = []
+
+    def get_indexes_of_first_pair():
+        servers_by_vlans = dict()
+        for idx, server in enumerate(accumulator):
+            try:
+                my_vlan = server['VLAN']
+                if my_vlan in servers_by_vlans:
+                    return [idx, servers_by_vlans[my_vlan]]
+                servers_by_vlans[my_vlan] = idx
+            except:
+                pass
+        return []
+
+    def term_generator(item):
+        item_index = len(accumulator)
+        accumulator.append(item)
+        return lambda: item_index in get_indexes_of_first_pair()
+
+    return term_generator
