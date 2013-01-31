96f93caaaf6f42e0623294b6978d03d5e1cbd934
Mate Lakat <mate.lakat@citrix.com>
2013-01-31 13:16:16 +0000

    do remote update

--- jenkins/devstack-xen/on-domu-stop-update-restart-cinder.sh
+++ jenkins/devstack-xen/on-domu-stop-update-restart-cinder.sh
@@ -6,9 +6,10 @@ pkill -HUP -f "/opt/stack/cinder/bin/cinder-volume" || true
 sleep 1
 
 cd cinder
+git remote update
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git pull
+git checkout origin/xenapinfs-glance-integration -B xenapinfs-glance-integration
 
 NL=`echo -ne '\015'`
 screen -S stack -p c-vol -X stuff "cd /opt/stack/cinder && /opt/stack/cinder/bin/cinder-volume --config-file /etc/cinder/cinder.conf$NL"
