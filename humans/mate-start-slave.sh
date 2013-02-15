fe41fe808f4882a7b08b1663601caa5368d60e16
Mate Lakat <mate.lakat@citrix.com>
2013-02-15 18:24:10 +0000

    Added some extra debug flags

--- humans/mate-start-slave.sh
+++ humans/mate-start-slave.sh
@@ -7,6 +7,7 @@ TOP_DIR=$(cd $(dirname "$0") && cd .. && pwd)
 SERVERNAME="${1-`echo 'Please specify as first parameter' && exit 5`}"
 
 ssh $SERVERNAME << EOF
+set -x
 xe vm-list name-label=slave | grep -q slave && exit 0
 mkdir -p /mnt/exported-vms
 mount -t nfs copper.eng.hq.xensource.com:/exported-vms /mnt/exported-vms
--- humans/mate-stop-slave.sh
+++ humans/mate-stop-slave.sh
@@ -7,6 +7,7 @@ TOP_DIR=$(cd $(dirname "$0") && cd .. && pwd)
 SERVERNAME="${1-`echo 'Please specify as first parameter' && exit 5`}"
 
 ssh $SERVERNAME << EOF
+set -x
 xe vm-shutdown vm=slave
 xe vm-uninstall vm=slave force=true
 EOF
