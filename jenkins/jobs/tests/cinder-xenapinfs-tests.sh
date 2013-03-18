ee5dd1fd7a33f7c143b9d76e64f71cef756b5294
Mate Lakat <mate.lakat@citrix.com>
2013-03-18 14:53:18 +0000

    Add apt-get update, apt-get upgrade

--- jenkins/jobs/tests/cinder-xenapinfs-tests.sh
+++ jenkins/jobs/tests/cinder-xenapinfs-tests.sh
@@ -12,6 +12,8 @@ export BRANCH="$5"
 
 export DEBIAN_FRONTEND=noninteractive
 
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -qy git nfs-common python-virtualenv blktap-utils python-dev
 sudo mkdir -p /mnt/nfsdir
 sudo mount -t nfs "${NFSSERVER}:${NFSPATH}" /mnt/nfsdir
