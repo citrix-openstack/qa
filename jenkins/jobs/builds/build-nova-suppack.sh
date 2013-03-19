220faa095204e761a4874bbd7a2d20717350794e
Mate Lakat <mate.lakat@citrix.com>
2013-03-19 14:29:14 +0000

    Update system, and install git

--- jenkins/jobs/builds/build-nova-suppack.sh
+++ jenkins/jobs/builds/build-nova-suppack.sh
@@ -1,5 +1,13 @@
 set -eux
 
+# Update system and install dependencies
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get -qy upgrade
+sudo apt-get install -qy git
+
+# Create suppack
 GITREPO="$1"
 git clone "$GITREPO"
 cd nova
