d47cde6897c6c06c7bcea0d0fe6a6d731178dc1c
Mate Lakat <mate.lakat@citrix.com>
2013-03-20 17:35:27 +0000

    Destroy extra vdis

--- jenkins/jobs/xslib/add-extra-hdd-to-devstack.sh
+++ jenkins/jobs/xslib/add-extra-hdd-to-devstack.sh
@@ -1,6 +1,19 @@
 set -eux
 
 DEVSTACKVM=$(xe vm-list name-label=DevStackOSDomU --minimal)
+
+for VBD in $(xe vbd-list vm-uuid=$DEVSTACKVM --minimal |  sed -e 's/,/ /g')
+do
+    DEVICE=$(xe vbd-param-get uuid=$VBD param-name=device)
+    if [ "$DEVICE" != "xvda" ]
+    then
+        xe vbd-unplug uuid=$VBD
+        VDI=$(xe vbd-param-get uuid=$VBD param-name=vdi-uuid)
+        xe vbd-destroy uuid=$VBD
+        xe vdi-destroy uuid=$VDI
+    fi
+done
+
 LOCALSR=$(xe sr-list name-label="Local storage" --minimal)
 EXTRA_VDI=$(xe vdi-create name-label=extra-disk-for-os-volumes virtual-size=20GiB sr-uuid=$LOCALSR type=user)
 EXTRA_VBD=$(xe vbd-create vm-uuid=$DEVSTACKVM vdi-uuid=$EXTRA_VDI device=autodetect)
