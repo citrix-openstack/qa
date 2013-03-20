9204e553481f5dde2f23dc2428171726b08d526a
Mate Lakat <mate.lakat@citrix.com>
2013-03-20 17:36:57 +0000

    Ignore errors if stack-volumes does not exist

--- jenkins/jobs/oslib/setup-extra-hdd.sh
+++ jenkins/jobs/oslib/setup-extra-hdd.sh
@@ -2,7 +2,7 @@ set -exu
 
 cd devstack
 ./unstack.sh || true
-sudo vgremove stack-volumes
+sudo vgremove stack-volumes || true
 sudo pvcreate /dev/xvdb
 sudo vgcreate stack-volumes /dev/xvdb
 ./stack.sh 2>&1 >>stacklog </dev/null
