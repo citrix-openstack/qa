9bf0435ca5374c62b2170f0860fab2635a543546
Mate Lakat <mate.lakat@citrix.com>
2013-03-28 12:11:08 +0000

    Cherry pick the patch for 1161356

--- jenkins/jobs/tests/live-block-migration-tempest.sh
+++ jenkins/jobs/tests/live-block-migration-tempest.sh
@@ -2,6 +2,10 @@
 set -exu
 
 cd tempest
+
+# Apply the patch
+git fetch https://review.openstack.org/openstack/tempest refs/changes/09/25609/1 && git cherry-pick FETCH_HEAD || true
+
 sed -i \
 -e 's/^live_migration_available.*/live_migration_available = True/g' \
 -e 's/^use_block_migration_for_live_migration.*/use_block_migration_for_live_migration = True/g' \
