6b2ed63fb795ebd852c9fb9aa121d8784823c9a1
Mate Lakat <mate.lakat@citrix.com>
2013-06-03 10:07:23 +0100

    Updating CI script to include the latest patches

--- xenserver-quantum-devstack.sh
+++ xenserver-quantum-devstack.sh
@@ -60,9 +60,8 @@ create_branch \
     "https://github.com/openstack-dev/devstack.git" \
     "git@github.com:$GITHUB_USER/devstack.git" \
     "$devstack_branch" << EOF
-git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/16/30416/3 && git cherry-pick FETCH_HEAD
-git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/03/30703/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/35/31035/3 && git cherry-pick FETCH_HEAD
+git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/35/31035/4 && git cherry-pick FETCH_HEAD
+git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/77/31177/3 && git cherry-pick FETCH_HEAD
 EOF
 
 # Create custom quantum branch
