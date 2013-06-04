51e4b4920028b287d89c413ff7d18343aa62fafb
Mate Lakat <mate.lakat@citrix.com>
2013-06-04 17:54:03 +0100

    Backup

--- install-smoke.sh
+++ install-smoke.sh
@@ -3,6 +3,7 @@
 # sudo apt-get update
 # sudo apt-get install -qy rake libxslt1-dev git
 
+# TODO: kytoon needs some fixes
 # sudo gem install kytoon
 # touch ~/.kytoon.conf
 
@@ -17,7 +18,10 @@
 
 set -eux
 
-XENSERVER=192.168.32.125
+# TODO
+XENSERVER=192.168.32.114
+SMOKE_ROOT_URL="http://copper.eng.hq.xensource.com/smoke-images"
+
 
 function xenserver()
 {
@@ -28,14 +32,45 @@ ssh -q \
     "root@$XENSERVER" bash -s --
 }
 
-SR=$(echo "xe sr-list type=ext --minimal" | xenserver)
-echo "mkdir -p /var/run/sr-mount/$SR/smoke-images" | xenserver
-echo "[ -h /images ] || ln -s /var/run/sr-mount/$SR/smoke-images /images" | xenserver
+if ! echo "true" | xenserver; then
+    ssh-copy-id -i ~/.ssh/id_rsa.pub root@$XENSERVER
+fi
+
+if ! echo "[ -h /images ]" | xenserver; then
+    SR=$(echo "xe sr-list type=ext --minimal" | xenserver)
+    echo "mkdir -p /var/run/sr-mount/$SR/smoke-images" | xenserver
+    echo "[ -h /images ] || ln -s /var/run/sr-mount/$SR/smoke-images /images" | xenserver
+fi
 
 for fname in fedora18-buildbox.xva fedora18-db.xva squeeze-agent-0.0.1.31.ova; do
+    echo -n "$fname"
     xenserver << EOF
-set -exu
+set -eu
 [ -e /images/$fname ] || \\
-wget -qO /images/$fname http://copper.eng.hq.xensource.com/smoke-images/$fname
+wget -qO /images/$fname $SMOKE_ROOT_URL/$fname
 EOF
+    echo " done!"
 done
+
+NET=$(echo "xe network-list name-label=smokeinternal --minimal" | xenserver)
+if [ -z "$NET" ]; then
+    echo "xe network-create name-label=smokeinternal" | xenserver
+fi
+
+BRIDGE=$(echo "xe network-list name-label=smokeinternal params=bridge --minimal" | xenserver)
+
+if ! echo "[ -e ~/.ssh/id_rsa ]" | xenserver; then
+    echo "ssh-keygen -q -C root@hypervisor -N '' -f ~/.ssh/id_rsa" | xenserver
+fi
+
+# Install git in Dom0
+if [ -z $(echo "which git" | xenserver) ]; then
+    xenserver << EOF
+set -eux
+wget http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+rpm -Uvh epel-release-5-4.noarch.rpm
+yum -y install git
+rpm -ev epel-release
+rm -f epel-release-5-4.noarch.rpm
+EOF
+fi
