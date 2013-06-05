73331d32baf7cfc760c5a6772e7c25843ff34155
Mate Lakat <mate.lakat@citrix.com>
2013-06-05 09:40:56 +0100

    slave-starter allows the user to configure networking

---
+++ jenkins/jobs/create-devbox.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+REMOTELIB=$(cd $(dirname $(readlink -f "$0")) && cd remote && pwd)
+XSLIB=$(cd $(dirname $(readlink -f "$0")) && cd xslib && pwd)
+TESTLIB=$(cd $(dirname $(readlink -f "$0")) && cd tests && pwd)
+
+function print_usage_and_die
+{
+cat >&2 << EOF
+usage: $0 XENSERVERNAME
+
+Create a devbox on a XenServer - a router ubuntu VM
+
+positional arguments:
+ XENSERVERNAME     The name of the XenServer
+EOF
+exit 1
+}
+
+XENSERVERNAME="${1-$(print_usage_and_die)}"
+
+set -x
+
+SLAVE_IP=$(cat $XSLIB/start-slave.sh | "$REMOTELIB/bash.sh" "root@$XENSERVERNAME" "1=home")
--- jenkins/jobs/xslib/start-slave.sh
+++ jenkins/jobs/xslib/start-slave.sh
@@ -1,6 +1,64 @@
 #!/bin/bash
 set -eux
 
+NETWORKING="0=xenbr0,${1:-}"
+
+function resolve_to_network() {
+    local name_or_bridge
+    local result
+
+    name_or_bridge="$1"
+
+    result=$(xe network-list name-label=$1 --minimal)
+    if [ -z "$result" ]; then
+        result=$(xe network-list bridge=$1 --minimal)
+    fi
+
+    if [ -z "$result" ]; then
+        echo "No network found with name-label/bridge $name_or_bridge" >&2
+        exit 1
+    fi
+
+    echo "$result"
+}
+
+function wipe_networking() {
+    local vm
+
+    vm=$1
+
+    IFS=","
+    for vif in $(xe vif-list vm-uuid=$vm --minimal); do
+        xe vif-destroy uuid=$vif
+    done
+    unset IFS
+}
+
+function setup_networking() {
+    local network_configs
+    local vm
+
+    vm="$1"
+    network_configs="$2"
+
+    local netname
+    local device
+    local net
+
+    IFS=","
+    for netconfig in $network_configs; do
+        device=$(echo $netconfig | cut -d"=" -f 1)
+        netname=$(echo $netconfig | cut -d"=" -f 2)
+
+        net=$(resolve_to_network $netname)
+        if ! xe vif-create device=$device vm-uuid=$vm network-uuid=$net >/dev/null; then
+            echo "Failed to create network interface" >&2
+            exit 1
+        fi
+    done
+    unset IFS
+}
+
 if [ -z "$(xe snapshot-list name-label=slave-fresh --minimal)" ]
 then
     xe vm-uninstall vm=slave force=true || true
@@ -18,7 +76,10 @@ xe snapshot-revert snapshot-uuid=$SNAP
 
 VM=$(xe vm-list name-label=slave --minimal)
 
-xe vm-start vm=slave
+wipe_networking $VM
+setup_networking $VM "$NETWORKING"
+
+xe vm-start uuid=$VM
 
 while [ "$(xe vm-param-get uuid=$VM param-name=power-state)" != "running" ];
 do
