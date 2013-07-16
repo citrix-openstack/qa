67fe99ec19857c69d615f864371f11b403d63fd9
Mate Lakat <mate.lakat@citrix.com>
2013-07-16 09:49:27 +0100

    Amend job generator

--- generate-citrix-job.sh
+++ generate-citrix-job.sh
@@ -2,14 +2,41 @@
 
 set -eux
 
-. lib/functions
+function print_usage_and_die
+{
+cat >&2 << EOF
+usage: $0 BRANCH_NAME [SETUP_TYPE]
 
-TEMPLATE_NAME="$1"
-BRANCH_NAME="$2"
+Generate a test script to the standard output
+
+positional arguments:
+ BRANCH_NAME      Name of the branch to be used
+ SETUP_TYPE       Type of setup, one of [nova-network, neutron] defaults to
+                  nova-network
+
+An example run:
+
+$0 build-1373962961 nova-network
+EOF
+exit 1
+}
+
+THIS_DIR=$(cd $(dirname "$0") && pwd)
+
+. $THIS_DIR/lib/functions
+
+TEMPLATE_NAME="$THIS_DIR/templates/tempest-smoke.sh"
+BRANCH_NAME="${1-$(print_usage_and_die)}"
+SETUP_TYPE="${2-"nova-network"}"
+
 
 EXTENSION_POINT="^# Additional Localrc parameters here$"
+EXTENSIONS=$(mktemp)
 
-REPOS_VERSIONS=$(mktemp)
+# Set ubuntu install proxy
+cat "$THIS_DIR/modifications/add-ubuntu-proxy-repos" >> $EXTENSIONS
+
+# Set custom repos
 {
     generate_repos | while read repo_record; do
         echo "$(var_name "$repo_record")=$(dst_repo "$repo_record")"
@@ -17,12 +44,19 @@ REPOS_VERSIONS=$(mktemp)
     done
     cat << EOF
 NOVA_ZIPBALL_URL="http://gold.eng.hq.xensource.com/git/internal/builds/nova/zipball/$BRANCH_NAME"
-NEUTRON_ZIPBALL_URL="http://gold.eng.hq.xensource.com/git/internal/builds/quantum/zipball/$BRANCH_NAME"
+NEUTRON_ZIPBALL_URL="http://gold.eng.hq.xensource.com/git/internal/builds/neutron/zipball/$BRANCH_NAME"
 EOF
-} > "$REPOS_VERSIONS"
+} > "$EXTENSIONS"
 
+# Configure neutron if needed
+if [ "$SETUP_TYPE" == "neutron" ]; then
+    cat "$THIS_DIR/modifications/use-neutron" >> $EXTENSIONS
+fi
+
+# Extend template
 sed \
-    -e "/$EXTENSION_POINT/r  modifications/add-ubuntu-proxy-repos" \
-    -e "/$EXTENSION_POINT/r  $REPOS_VERSIONS" \
+    -e "/$EXTENSION_POINT/r  $EXTENSIONS" \
     -e "s,^\(DEVSTACK_TGZ=\).*,\1http://gold.eng.hq.xensource.com/git/internal/builds/devstack/archive/$BRANCH_NAME.tar.gz,g" \
     "$TEMPLATE_NAME"
+
+rm -f "$EXTENSIONS"
