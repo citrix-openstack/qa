46a572c08262e0dba9408ae582786c88f187a242
Mate Lakat <mate.lakat@citrix.com>
2013-07-18 21:57:08 +0100

    Added transfervm ci job

---
+++ jenkins/jobs/run-transfervm-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+
+REMOTELIB=$(cd $(dirname $(readlink -f "$0")) && cd remote && pwd)
+XSLIB=$(cd $(dirname $(readlink -f "$0")) && cd xslib && pwd)
+
+function print_usage_and_die
+{
+cat >&2 << EOF
+usage: $0 XENSERVERNAME
+
+Run transfervm build on a new slave
+
+positional arguments:
+ XENSERVERNAME     The name of the XenServer
+EOF
+exit 1
+}
+
+XENSERVERNAME="${1-$(print_usage_and_die)}"
+
+set -x
+
+SLAVE_IP=$(cat $XSLIB/start-slave.sh | "$REMOTELIB/bash.sh" "root@$XENSERVERNAME")
+
+"$REMOTELIB/bash.sh" "ubuntu@$SLAVE_IP" << END_OF_TVM_TESTS
+set -eux
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get -qy update
+sudo apt-get -qy dist-upgrade
+sudo apt-get -qy install git
+
+git clone https://github.com/matelakat/transfervm transfervm
+END_OF_TVM_TESTS
+
+echo "$SLAVE_IP"
