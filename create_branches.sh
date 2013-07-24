14ca1e10034709c3250cb0fff53d32d352e073eb
Mate Lakat <mate.lakat@citrix.com>
2013-07-24 11:55:15 +0100

    push repos only if something changed

--- create_branches.sh
+++ create_branches.sh
@@ -15,25 +15,32 @@ pull_status_repo status
 PREV_BRANCH=$(read_latest_branch status)
 
 create_local_build_branch "$BRANCH_NAME"
-with_all_repos git push --tags --quiet build "$BRANCH_NAME"
+
+UPDATED="no"
 
 if [ -z "$PREV_BRANCH" ]; then
+    with_all_repos git push --tags --quiet build "$BRANCH_NAME"
     echo "$BRANCH_NAME" | write_latest_branch status
     push_status_repo status
+    UPDATED="yes"
 else
     if ! print_updated_repos "$PREV_BRANCH" "$BRANCH_NAME" | diff -u /dev/null -; then
+        with_all_repos git push --tags --quiet build "$BRANCH_NAME"
         echo "$BRANCH_NAME" | write_latest_branch status
         push_status_repo status
+        UPDATED="yes"
     fi
 fi
 
 
-CITRIX_BRANCH_NAME="ctx-$(date +%s)"
-create_local_build_branch "$CITRIX_BRANCH_NAME"
-cd NOVA_REPO
-git fetch https://review.openstack.org/openstack/nova refs/changes/41/38441/1 && git cherry-pick FETCH_HEAD
-cd ..
-cd DEVSTACK_REPO
-git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/44/38444/2 && git cherry-pick FETCH_HEAD
-cd ..
-with_all_repos git push --tags --quiet build "$CITRIX_BRANCH_NAME"
+if [ "$UPDATED" == "yes" ]; then
+    CITRIX_BRANCH_NAME="ctx-$(date +%s)"
+    create_local_build_branch "$CITRIX_BRANCH_NAME"
+    cd NOVA_REPO
+    git fetch https://review.openstack.org/openstack/nova refs/changes/41/38441/1 && git cherry-pick FETCH_HEAD
+    cd ..
+    cd DEVSTACK_REPO
+    git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/44/38444/2 && git cherry-pick FETCH_HEAD
+    cd ..
+    with_all_repos git push --tags --quiet build "$CITRIX_BRANCH_NAME"
+fi
