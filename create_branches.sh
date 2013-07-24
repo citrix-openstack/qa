7c08bb5c1ed49ef65ee88666572a29ebfc81c4ac
Mate Lakat <mate.lakat@citrix.com>
2013-07-24 11:22:29 +0100

    Create citrix branches

--- create_branches.sh
+++ create_branches.sh
@@ -15,6 +15,7 @@ pull_status_repo status
 PREV_BRANCH=$(read_latest_branch status)
 
 create_local_build_branch "$BRANCH_NAME"
+with_all_repos git push --tags --quiet build "$BRANCH_NAME"
 
 if [ -z "$PREV_BRANCH" ]; then
     echo "$BRANCH_NAME" | write_latest_branch status
@@ -25,3 +26,14 @@ else
         push_status_repo status
     fi
 fi
+
+
+CITRIX_BRANCH_NAME="ctx-$(date +%s)"
+create_local_build_branch "$CITRIX_BRANCH_NAME"
+cd NOVA_REPO
+git fetch https://review.openstack.org/openstack/nova refs/changes/41/38441/1 && git cherry-pick FETCH_HEAD
+cd ..
+cd DEVSTACK_REPO
+git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/44/38444/1 && git cherry-pick FETCH_HEAD
+cd ..
+with_all_repos git push --tags --quiet build "$CITRIX_BRANCH_NAME"
