d1c8b959f198c50e912c51d6051aad2cd9bdd63b
Mate Lakat <mate.lakat@citrix.com>
2013-07-29 16:50:37 +0100

    Fix xva builder CI job

--- jenkins/jobs/build-devstack-xva.sh
+++ jenkins/jobs/build-devstack-xva.sh
@@ -2,14 +2,14 @@
 
 set -eu
 
-SCRIPTDIR=$(cd $(dirname $(readlink -f "$0")) && cd .. && cd devstack-xen && pwd)
 XSLIB=$(cd $(dirname $(readlink -f "$0")) && cd xslib && pwd)
 BUILDLIB=$(cd $(dirname $(readlink -f "$0")) && cd builds && pwd)
+REMOTELIB=$(cd $(dirname $(readlink -f "$0")) && cd remote && pwd)
 
 function print_usage_and_die
 {
 cat >&2 << EOF
-usage: $0 SERVERNAME GITREPO DDK_ROOT_URL GITBRANCH
+usage: $0 SERVERNAME
 
 Build Nova Supplemental Pack
 
@@ -21,23 +21,14 @@ exit 1
 
 SERVERNAME="${1-$(print_usage_and_die)}"
 
-function start_slave
-{
-    "$SCRIPTDIR/run-on-xenserver.sh" "$SERVERNAME" "$XSLIB/start-slave.sh"
-}
-
-function run_on
-{
-    THE_IP="$1"
-    SCRIPT="$2"
-    shift 2
-
-    cat "$SCRIPT" | ssh -q -o Batchmode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@$THE_IP" bash -s -- "$@"
-}
-
 echo "Spinning up virtual machine"
-SLAVE_IP=$(start_slave)
+SLAVE_IP=$(cat $XSLIB/start-slave.sh |
+    "$REMOTELIB/bash.sh" "root@$SERVERNAME")
+
 echo "Starting job on $SLAVE_IP"
-run_on $SLAVE_IP "$BUILDLIB/builds/devstack-xva/build.sh"
+cat "$BUILDLIB/builds/devstack-xva/build.sh" |
+    "$REMOTELIB/bash.sh" "ubuntu@$SLAVE_IP"
+
 echo "Copying build result to copper"
-scp ubuntu@$SLAVEIP:output.xva jenkinsoutput@copper.eng.hq.xensource.com:/usr/share/nginx/www/builds/devstack.xva
+scp ubuntu@$SLAVE_IP:output.xva \
+    jenkinsoutput@copper.eng.hq.xensource.com:/usr/share/nginx/www/builds/devstack.xva
