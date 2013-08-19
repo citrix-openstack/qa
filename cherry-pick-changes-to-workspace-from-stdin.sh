00ca93e6d13ac39e99b16ed55a0e56db0e15fde4
Mate Lakat <mate.lakat@citrix.com>
2013-08-19 13:19:28 +0100

    Added conflict resolution mechanism

--- cherry-pick-changes-to-workspace-from-stdin.sh
+++ cherry-pick-changes-to-workspace-from-stdin.sh
@@ -2,7 +2,28 @@
 
 set -eu
 
+function print_usage_and_die
+{
+cat >&2 << EOF
+usage: $0 CONFLICT_SCRIPT_DIR
+
+Cherry-pick citrix changes with additional conflict resolution.
+
+positional arguments:
+ CONFLICT_SCRIPT_DIR  Directory, where conflict resolution scripts live
+
+EOF
+exit 1
+}
+
+
 . lib/functions
+
+THIS_DIR="$(cd $(dirname $(readlink -f $0)) && pwd)"
+CONFLICT_PATCHES_DIR="${1-$(print_usage_and_die)}"
+
+echo "Working directory   : [$THIS_DIR]"
+echo "Conflict patches dir: [$CONFLICT_PATCHES_DIR]"
 
 assert_no_new_repos
 
@@ -17,6 +38,7 @@ while read change; do
     set -e
     user=$(echo "$change" | cut -d"/" -f 1)
     repo=$(echo "$change" | cut -d" " -f 1 | cut -d"/" -f 2)
+    change_number=$(echo "$change" | cut -d" " -f 2 | cut -d"/" -f 4)
     changeref=$(echo "$change" | cut -d" " -f 2)
     if grep -q "$user $repo" "$tmpfile"; then
         echo "[CHERRY-PICK-START] $change"
@@ -25,9 +47,18 @@ while read change; do
             if git fetch $(source_repo "$repo_record") $changeref && git cherry-pick FETCH_HEAD; then
                 echo "[CHERRY-PICK-OK]"
             else
-                echo "[CHERRY-PICK-FAIL]"
+                echo "[CHERRY-PICK-FAIL] Printing diff"
                 git diff
-                exit 1
+                resolution_script="$CONFLICT_PATCHES_DIR/$change_number"
+                echo "[CHERRY-PICK-FAIL] looking for resolution script as $resolution_script"
+                if [ -x  "$resolution_script" ]; then
+                    echo "[CHERRY-PICK-FAIL] Applying script $resolution_script"
+                    $resolution_script
+                    echo "[CHERRY-PICK-FAIL] script done"
+                else
+                    echo "[CHERRY-PICK-FAIL] No resolution found"
+                    exit 1
+                fi
             fi
         cd ..
     else
