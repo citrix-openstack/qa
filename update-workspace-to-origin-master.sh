924345bf456a3b9aee03d253ef5deb563ec7fd0b
Mate Lakat <mate.lakat@citrix.com>
2013-08-30 11:23:46 +0100

    Fetch tags

--- update-workspace-to-origin-master.sh
+++ update-workspace-to-origin-master.sh
@@ -5,7 +5,7 @@ echo "Creating workspace..."
 ./create-workspace.sh
 echo "Resetting workspace..."
 ./reset-workspace.sh
-echo "Fetch latest changes from origin remotes..."
-./with-all-repos-in-workspace.sh git fetch origin refs/heads/master
-echo "Checking out FETCH_HEAD as tmpbranch"
-./with-all-repos-in-workspace.sh git checkout FETCH_HEAD -B tmpbranch
+echo "Fetch remotes..."
+./with-all-repos-in-workspace.sh git fetch --all
+echo "Check out origin/master as a new branch..."
+./with-all-repos-in-workspace.sh git checkout origin/master -B tmpbranch --no-track
