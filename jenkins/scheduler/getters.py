5fbd039a715bd782e812de3853fb8f5daa9f2a6c
Bob Ball <bob.ball@citrix.com>
2013-09-02 15:54:15 +0100

    Add ability to set reason for lock

--- jenkins/scheduler/getters.py
+++ jenkins/scheduler/getters.py
@@ -5,11 +5,11 @@ import string
 from selectors import first
 
 
-def wait_for(selector_factory):
+def wait_for(selector_factory, reason):
     lock = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
 
     while True:
-        items = database.lock_items('server.database', lock, selector_factory())
+        items = database.lock_items('server.database', lock, selector_factory(), reason)
         if items:
             break
         time.sleep(1)
@@ -17,10 +17,10 @@ def wait_for(selector_factory):
     return items, lock
 
 
-def wait_for_first(condition):
+def wait_for_first(condition, reason):
     selector_factory = lambda: first(condition)
 
-    items, lock = wait_for(selector_factory)
+    items, lock = wait_for(selector_factory, reason)
 
     assert len(items) == 1
 
