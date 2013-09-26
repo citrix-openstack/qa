020924958d834bda0755b5297e538b6f752f0fdc
Mate Lakat <mate.lakat@citrix.com>
2013-09-26 15:00:19 +0100

    refactor - THIS_DIR variable

--- jenkins/jobs/xva-build.sh
+++ jenkins/jobs/xva-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+THISDIR=$(cd $(dirname $(readlink -f "$0")) && pwd)
+
 function run_on
 {
     THE_IP="$1"
@@ -15,11 +17,11 @@ function run_on
 SLAVE_IP=$("jenkins/devstack-xen/run-on-xenserver.sh" "$HOST" "jenkins/jobs/xslib/start-slave.sh")
 
 echo "Building Devstack XVA"
-run_on "$SLAVE_IP" "jenkins/jobs/builds/build-devstack-xva-online-stage1.sh" "$HOST" "$XenServerPassword"
-run_on "$SLAVE_IP" "jenkins/jobs/builds/build-devstack-xva-online-stage2.sh" "$HOST" "$XenServerPassword"
+run_on "$SLAVE_IP" "$THISDIR/builds/build-devstack-xva-online-stage1.sh" "$HOST" "$XenServerPassword"
+run_on "$SLAVE_IP" "$THISDIR/builds/build-devstack-xva-online-stage2.sh" "$HOST" "$XenServerPassword"
 
 echo "Building Nova suppack"
-run_on "$SLAVE_IP" "jenkins/jobs/builds/build-nova-suppack.sh" "https://github.com/openstack/nova.git" "http://copper.eng.hq.xensource.com/builds/ddk-xs6_2.tgz" "master"
+run_on "$SLAVE_IP" "$THISDIR/builds/build-nova-suppack.sh" "https://github.com/openstack/nova.git" "http://copper.eng.hq.xensource.com/builds/ddk-xs6_2.tgz" "master"
 
 echo "Qualifying Devstack XVA with Nova suppack"
-run_on "$SLAVE_IP" "jenkins/jobs/builds/qualify-devstack-xva.sh" "$HOST" "$XenServerPassword" "~/devstack.xva" "citrix" "~/suppack/novaplugins.iso"
+run_on "$SLAVE_IP" "$THISDIR/builds/qualify-devstack-xva.sh" "$HOST" "$XenServerPassword" "~/devstack.xva" "citrix" "~/suppack/novaplugins.iso"
