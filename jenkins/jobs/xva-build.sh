91d07f55a6ff5f13ae0d082d94395541a4296e69
Mate Lakat <mate.lakat@citrix.com>
2013-09-26 16:05:05 +0100

    Add get-worker.sh
    This script is a copy of start-slave.sh except it will return
    with the full string that could be used for remote bashing.
    this way it's not relevant what is the username.

--- jenkins/jobs/xva-build.sh
+++ jenkins/jobs/xva-build.sh
@@ -27,20 +27,20 @@ function parse_parameters() {
 parse_parameters $@
 
 
-SLAVE_IP=$(run_bash_script_on "root@$HOST" "$THISDIR/xslib/start-slave.sh")
+WORKER=$(run_bash_script_on "root@$HOST" "$THISDIR/xslib/get-worker.sh")
 
-echo "Slave IP address: $SLAVE_IP"
+echo "Worker: $WORKER"
 
 echo "Building Devstack XVA" | log_info
-run_bash_script_on "ubuntu@$SLAVE_IP" \
+run_bash_script_on "$WORKER" \
     "$THISDIR/builds/build-devstack-xva-online-stage1.sh" "$HOST" "$XenServerPassword"
-run_bash_script_on "ubuntu@$SLAVE_IP" \
+run_bash_script_on "$WORKER" \
     "$THISDIR/builds/build-devstack-xva-online-stage2.sh" "$HOST" "$XenServerPassword"
 
 echo "Building Nova suppack" | log_info
-run_bash_script_on "ubuntu@$SLAVE_IP" \
+run_bash_script_on "$WORKER" \
     "$THISDIR/builds/build-nova-suppack.sh" "https://github.com/openstack/nova.git" "http://copper.eng.hq.xensource.com/builds/ddk-xs6_2.tgz" "master"
 
 echo "Qualifying Devstack XVA with Nova suppack" | log_info
-run_bash_script_on "ubuntu@$SLAVE_IP" \
+run_bash_script_on "$WORKER" \
     "$THISDIR/builds/qualify-devstack-xva.sh" "$HOST" "$XenServerPassword" "~/devstack.xva" "citrix" "~/suppack/novaplugins.iso"
