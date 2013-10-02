ca72182bebf2c207475b9f6f04240dc79465949d
Mate Lakat <mate.lakat@citrix.com>
2013-10-02 16:57:14 +0100

    Fetch only origin

--- update-workspace-to-origin-master.sh
+++ update-workspace-to-origin-master.sh
@@ -5,7 +5,7 @@ echo "Creating workspace..."
 ./create-workspace.sh
 echo "Resetting workspace..."
 ./reset-workspace.sh
-echo "Fetch remotes..."
-./with-all-repos-in-workspace.sh git fetch --all
+echo "Fetch origin..."
+./with-all-repos-in-workspace.sh git fetch origin
 echo "Check out origin/master as a new branch..."
 ./with-all-repos-in-workspace.sh git checkout origin/master -B tmpbranch --no-track
