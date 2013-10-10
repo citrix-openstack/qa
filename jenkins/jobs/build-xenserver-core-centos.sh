eb269c26d95142598b6d7693260894882a2eeee0
Bob Ball <bob.ball@citrix.com>
2013-10-10 13:35:11 +0100

    Added job to build xenserver-core on centos

---
+++ jenkins/jobs/build-xenserver-core-centos.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -eux
+
+REMOTELIB=$(cd $(dirname $(readlink -f "$0")) && cd remote && pwd)
+XSLIB=$(cd $(dirname $(readlink -f "$0")) && cd xslib && pwd)
+TESTLIB=$(cd $(dirname $(readlink -f "$0")) && cd tests && pwd)
+
+function print_usage_and_die
+{
+cat >&2 << EOF
+usage: $0 HOSTNAME
+
+Build xenserver-core packages
+
+positional arguments:
+ HOSTNAME     The name of the CentOS host on which we are going to compile
+EOF
+exit 1
+}
+
+HOSTNAME="${1-$(print_usage_and_die)}"
+
+"$REMOTELIB/bash.sh" "root@$HOSTNAME" << END_OF_XSCORE_BUILD_SCRIPT
+set -eux
+
+# Install epel so we can get mock
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+set +e
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+RET=\$?
+if [ \$RET -ne 0 -a \$RET -ne 2 ]; then
+  exit \$RET
+fi
+set -e
+
+yum install -y mock redhat-lsb-core
+
+getent passwd mock || useradd -g mock -d /home/mock -s /bin/bash -p \$(echo mock | openssl passwd -1 -stdin) mock
+
+# Allow mock to sudo
+cat >> /etc/sudoers << EOF_SUDOERS
+mock    ALL=(ALL)       NOPASSWD:ALL
+EOF_SUDOERS
+
+# The rest of the script needs to run as the mock user
+cat >> /home/mock/build.sh << EOF_BUILD_SCRIPT
+cd ~
+git clone git://github.com/xapi-project/xenserver-core.git
+cd xenserver-core
+
+./configure.sh
+./makemake.py > Makefile
+make
+EOF_BUILD_SCRIPT
+su - mock -c "bash /home/mock/build.sh"
+
+END_OF_XSCORE_BUILD_SCRIPT
--- jenkins/jobs/remote/bash.sh
+++ jenkins/jobs/remote/bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 function print_usage_and_die
 {
