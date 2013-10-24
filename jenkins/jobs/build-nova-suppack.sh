6aaa17e37b2a1279e457f734dd4f2d3a3a3bf6da
Mate Lakat <mate.lakat@citrix.com>
2013-10-24 17:05:25 +0100

    Fix suppack builder

--- jenkins/jobs/build-nova-suppack.sh
+++ jenkins/jobs/build-nova-suppack.sh
@@ -30,6 +30,7 @@ DDK_ROOT_URL="${3-$(print_usage_and_die)}"
 GITBRANCH="${4-$(print_usage_and_die)}"
 
 echo "Spinning up virtual machine"
-WORKER=$(cat $XSLIB/get_worker.sh | remote_bash "root@$XENSERVERNAME")
+WORKER=$(cat $XSLIB/get_worker.sh | remote_bash "root@$SERVERNAME")
 echo "Starting job on $SLAVE_IP"
-run_bash_script_on $SLAVE_IP "$BUILDLIB/build-nova-suppack.sh" "$GITREPO" "$DDK_ROOT_URL" "$GITBRANCH"
+run_bash_script_on "$WORKER" \
+    "$BUILDLIB/build-nova-suppack.sh" "$GITREPO" "$DDK_ROOT_URL" "$GITBRANCH"
