049e4044aaba0d67ab44ae0012a65ac7f38bea60
Mate Lakat <mate.lakat@citrix.com>
2013-10-27 22:38:12 +0000

    Fix typo

--- jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
+++ jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 
-sh guard@silicon lock-get-single-server --reason $BUILD_URL > "${BUILD_NUMBER}.properties"
+ssh guard@silicon lock-get-single-server --reason $BUILD_URL > "${BUILD_NUMBER}.properties"
 . "$(pwd)/${BUILD_NUMBER}.properties"
