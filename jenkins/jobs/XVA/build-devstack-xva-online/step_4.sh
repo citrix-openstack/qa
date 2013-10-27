a6f7a8cff103f9b6eb2ecfe6c1c19e215d7f9a29
Mate Lakat <mlakat@mlakat.hu>
2013-10-27 20:44:37 +0000

    xva filenames are passed as parameters
    Also clean-up actions removed, and a library was introduced to have some
    help with generating tha paths.

--- jenkins/jobs/XVA/build-devstack-xva-online/step_4.sh
+++ jenkins/jobs/XVA/build-devstack-xva-online/step_4.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -eux
 
+THISDIR="$(cd "$(dirname $0)" && pwd)"
+. "${THISDIR}/../lib.sh"
+
 if [ "$PUBLISH_RESULTS" = "false" ]; then
   echo "No publish was requested, exiting"
   exit 0
 fi
 
-
-. "$(pwd)/${BUILD_NUMBER}.saveenv"
-
 echo "Mirroring build result from copper to public http server"
-ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost wget http://10.219.13.54/builds/novaplugins-$TODAY.iso -O $PublicHttpServerOpenStackPath/novaplugins-$TODAY.iso
-ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost wget http://10.219.13.54/builds/devstack-$TODAY.xva -O $PublicHttpServerOpenStackPath/devstack-$TODAY.xva
-echo "Tidying up old builds on public http server"
-ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost rm -f $PublicHttpServerOpenStackPath/novaplugins-$SIXDAYSAGO.iso || true
-ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost rm -f $PublicHttpServerOpenStackPath/devstack-$SIXDAYSAGO.xva || true
+ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost \
+    wget "http://10.219.13.54/builds/$(novaplugins_name $XVA_INTERNAL_NAME)" \
+    -O "$PublicHttpServerOpenStackPath/$(novaplugins_name $XVA_NAME)"
+
+ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost \
+    wget "http://10.219.13.54/builds/$(xva_name $XVA_INTERNAL_NAME)" \
+    -O "$PublicHttpServerOpenStackPath/$(xva_name $XVA_NAME)"
+
+echo "Remove internal builds"
+ssh $INTERNAL_HTTP_USER_HOST rm -f \
+    $(internal_novaplugins_path $XVA_INTERNAL_NAME) \
+    $(internal_xva_path $XVA_INTERNAL_NAME)
 
 echo "The published build result is now located at:"
-echo "    $PublicHttpServerOpenStackLocation/novaplugins-$TODAY.iso"
-echo "    $PublicHttpServerOpenStackLocation/devstack-$TODAY.xva"
+echo "    $PublicHttpServerOpenStackLocation/$(novaplugins_name $XVA_NAME)"
+echo "    $PublicHttpServerOpenStackLocation/$(xva_name $XVA_NAME)"
--- jenkins/jobs/XVA/xva-build-external/step_1.sh
+++ jenkins/jobs/XVA/xva-build-external/step_1.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-
 set -eux
 
-if [ -z "$REF_NAME" ]; then
-  echo "No ref specified"
-  exit 1
-fi
+THISDIR="$(cd "$(dirname $0)" && pwd)"
+. "$THISDIR/../lib.sh"
 
-[ "$PUBLISH_REF_NAME" = "ctx-nova-network-smoke-latest" ]
+[ -n "$REF_NAME" ]
+[ -n "$DEVSTACK_INSTALLER_SCRIPT_URL" ]
 
+# This is a daily build
 TODAY=$(date +"%m_%d_%Y")
-TODAYS_URL="http://downloads.vmd.citrix.com/OpenStack/novaplugins-$TODAY.iso"
+XVA_NAME="${TODAY}"
+NOVAPLUGINS_URL="$PublicHttpServerOpenStackLocation/$(novaplugins_name $XVA_NAME)"
 
 # Let's see if a build is needed...
-if curl -sI "$TODAYS_URL" | grep -q 404; then
-  echo "No build found for today"
+if curl -sI "$NOVAPLUGINS_URL" | grep -q 404; then
+    echo "No build found for today"
 else
-  echo "Build found for today"
-  exit 1
+    echo "Build found for today"
+    exit 1
 fi
 
-./generate-citrix-job.sh "$REF_NAME" nova-network |
-  ./change-repos-of-generated-jobs-to-public.sh |
-  ./remove-ubunutu-install-settings.sh > ${BUILD_NUMBER}.sh
+# Edit the installer script, and replace the repos with the public ones.
+wget -qO - "$DEVSTACK_INSTALLER_SCRIPT_URL" |
+    ./change-repos-of-generated-jobs-to-public.sh |
+    ./remove-ubunutu-install-settings.sh > ${BUILD_NUMBER}.sh
 
-scp -o 'StrictHostKeyChecking no' ${BUILD_NUMBER}.sh jenkinsoutput@copper.eng.hq.xensource.com:/usr/share/nginx/www/builds/${BUILD_TAG}.sh
+# Copy the installer script to a web server
+scp ${BUILD_NUMBER}.sh "devstack_script_producer@unsteve.eng.hq.xensource.com:/devstack-scripts/${BUILD_TAG}.sh"
 
 cat > ${BUILD_NUMBER}.properties << EOF
-SETUPSCRIPT_URL=http://copper.eng.hq.xensource.com/builds/${BUILD_TAG}.sh
+SETUPSCRIPT_URL=http://unsteve.eng.hq.xensource.com/build-results/devstack-scripts/${BUILD_TAG}.sh
 NOVA_REPO=https://github.com/citrix-openstack-build/nova.git
 NOVA_BRANCH=${REF_NAME}
 JEOS_URL=http://copper.eng.hq.xensource.com/jeos/external-precise.xva
 PUBLISH_RESULTS=true
+XVA_NAME=${XVA_NAME}
+XVA_INTERNAL_NAME=${BUILD_TAG}
 EOF
