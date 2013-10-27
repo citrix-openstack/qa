1a63cb1e0175bc1a05d6ee8e49562eb9aabd4e4e
Mate Lakat <mlakat@mlakat.hu>
2013-10-27 21:26:41 +0000

    code formatting

--- jenkins/jobs/XVA/build-named-xva/step_1.sh
+++ jenkins/jobs/XVA/build-named-xva/step_1.sh
@@ -3,4 +3,5 @@ set -eux
 
 THISDIR="$(cd "$(dirname $0)" && pwd)"
 
-$THISDIR/../generate_parameters_for_xva_build_online.sh named ${BUILD_NUMBER}.sh
+$THISDIR/../generate_parameters_for_xva_build_online.sh \
+    named ${BUILD_NUMBER}.sh
