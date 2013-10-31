ff5ba03e3bdd12c11aa7782eab98aaa2300b6723
Mate Lakat <mate.lakat@citrix.com>
2013-10-31 17:09:06 +0000

    Added demo scripts

--- havana-demo/infinite_migration.sh
+++ havana-demo/infinite_migration.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -eu
 
-IMAGE_NAME="$1"
-
 cd /opt/stack/devstack
-
 
 set +u
 . openrc admin
@@ -12,15 +9,17 @@ set -u
 
 function start_vm() {
     local vm_name
+    local image_name
 
     vm_name="$1"
+    image_name="$2"
 
     if nova list | grep -q "$vm_name"; then
         echo "ERROR: instance [$vm_name] already exists!"
         exit 1
     fi
 
-    nova boot --image "$IMAGE_NAME" --flavor m1.tiny "$vm_name" > /dev/null
+    nova boot --image "$image_name" --flavor m1.tiny "$vm_name"
 }
 
 function list_hosts_forever() {
@@ -44,10 +43,6 @@ function wait_for_active() {
     
     vm_name="$1"
 
-    if nova list | grep "$vm_name" | grep -q ACTIVE; then
-        return
-    fi
-
     echo -n "Waiting for ACTIVE status"
     while ! nova list | grep "$vm_name" | grep -q ACTIVE; do
         echo -n "."
@@ -56,20 +51,112 @@ function wait_for_active() {
     echo "done"
 }
 
+function image_exists() {
+    local image_name
+
+    image_name="$1"
+
+    glance image-list | grep -q "$image_name"
+}
+
+function vm_exists() {
+    local vm_name
+
+    vm_name="$1"
+
+    nova list | grep -q "$vm_name"
+}
+
+function print_vm_ip() {
+    local vm_name
+
+    vm_name="$1"
+
+    nova show demo-instance | grep "private network" | tr -d " " | cut -d"|" -f3
+}
+
+function image_is_active() {
+    local image_name
+
+    image_name="$1"
+
+    glance image-list | grep "$image_name" | grep -q "active"
+}
+
+function remove_image() {
+    local image_name
+
+    image_name="$1"
+
+    if image_exists "$image_name"; then
+        glance image-delete "$image_name"
+
+        while image_exists "$image_name"; do
+            sleep 1
+        done
+    fi
+}
+
+function upload_image() {
+    local image_url
+    local image_name
+
+    image_url="$1"
+    image_name="$2"
+
+    glance image-create \
+        --disk-format=vhd \
+        --container-format=ovf \
+        --copy-from="$image_url" \
+        --is-public=True \
+        --name="$image_name"
+
+    while ! image_is_active "$image_name"; do
+        sleep 1
+    done
+}
+
+function remove_vm() {
+    local vm_name
+
+    vm_name="$1"
+
+    if vm_exists "$vm_name"; then
+        nova delete "$vm_name"
+    fi
+
+    while vm_exists "$vm_name"; do
+        sleep 1
+    done
+}
+
 
 MIGRATION_COUNTER=0
 SECONDS_AT_SERVER=10
 
-start_vm "demo-instance"
+
+remove_image "demo-image"
+
+upload_image \
+    "http://copper.eng.hq.xensource.com/havana-demo/streamer-coalesced.vhd.tgz" \
+    "demo-image"
+
+remove_vm "demo-instance"
+
+start_vm "demo-instance" "demo-image"
+
+wait_for_active "demo-instance"
+
 list_hosts_forever | while read host; do
-    wait_for_active "demo-instance"
     if vm_is_on_host "demo-instance" "$host"; then
         continue
     else
-        echo "Waiting $SECONDS_AT_SERVER seconds"
-        sleep $SECONDS_AT_SERVER
+        echo "Private address of the vm is: $(print_vm_ip demo-instance)"
         echo "Asking for live migration to [$host] $MIGRATION_COUNTER migrations so far"
         nova live-migration --block-migrate "demo-instance" "$host"
         MIGRATION_COUNTER=$(expr $MIGRATION_COUNTER + 1)
+        wait_for_active "demo-instance"
+        echo "Waiting $SECONDS_AT_SERVER seconds"
+        sleep $SECONDS_AT_SERVER
     fi
 done
---
+++ havana-demo/run-infinite-migration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eux
+
+[ -n "$HOST_2" ]
+
+DEVSTACK_2=$(ssh -q -o BatchMode=yes $HOST_2 bash -s -- << EOF
+set -eux
+wget -qO functions https://raw.github.com/openstack-dev/devstack/master/tools/xen/functions
+. functions
+find_ip_by_name DevStackOSDomU 0
+EOF
+)
+
+ssh -q -A -o BatchMode=yes stack@$DEVSTACK_2 bash -s -- << EOF
+set -eux
+wget -qO infinite_migration.sh https://github.com/citrix-openstack/qa/raw/master/havana-demo/infinite_migration.sh
+bash infinite_migration.sh
+EOF
