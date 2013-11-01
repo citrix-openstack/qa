06573cf792bdc75d6201834ddeb4dda37ca3f792
Mate Lakat <mate.lakat@citrix.com>
2013-11-01 10:11:03 +0000

    Print some info

--- havana-demo/run-infinite-migration.sh
+++ havana-demo/run-infinite-migration.sh
@@ -13,6 +13,22 @@ find_ip_by_name DevStackOSDomU 0
 EOF
 )
 
+set +x
+cat << EOF
+
+### HAVANA DEMO ###
+
+Running two xenserver-core compute nodes with a webserver migrating back and
+forth in an infinite loop.
+
+The webserver is serving two video files:
+
+http://$DEVSTACK_2:1235/trailer_480p.mov
+http://$DEVSTACK_2:1235/the-xen-movie.mp4
+
+EOF
+set -x
+
 ssh -q -A -o BatchMode=yes stack@$DEVSTACK_2 bash -s -- << EOF
 set -eux
 wget -qO infinite_migration.sh https://github.com/citrix-openstack/qa/raw/master/havana-demo/infinite_migration.sh
