176e1651754b96728117acbda907c79f841bcf1a
Mate Lakat <mate.lakat@citrix.com>
2014-01-13 11:17:28 +0000

    Kill ssh agent on error

---
+++ jenkins/jobs/update-testing-xva.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eux
+
+[ -n "$PublicHttpServerUserAndHost" ]
+[ -n "$PublicHttpServerOpenStackPath" ]
+[ -n "$PrivateKeyToPublicHttpServer" ]
+
+eval $(ssh-agent) || { ssh-agent -k; exit 1; }
+ssh-add "$PrivateKeyToPublicHttpServer" || { ssh-agent -k; exit 1; }
+
+{
+    for dependency in worker-vms remote-bash; do
+        [ -e $dependency ] \
+        && ( cd $dependency; git pull ) \
+        || git clone https://github.com/citrix-openstack/$dependency
+
+        export PATH=$PATH:$(pwd)/$dependency/bin
+    done
+
+    [ -e "openstack-xenapi-testing-xva" ] \
+        && ( cd openstack-xenapi-testing-xva; git pull ) \
+        || git clone https://github.com/citrix-openstack/openstack-xenapi-testing-xva
+
+    cd openstack-xenapi-testing-xva
+
+    bin/cloud-xva-create
+} || { ssh-agent -k; exit 1; }
