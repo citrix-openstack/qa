331b4bc57d2130458382a90c2b68042b4844a22f
Bob Ball <bob.ball@citrix.com>
2014-01-17 13:48:43 +0000

    Fix the correct glob!!!

--- jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
+++ jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
@@ -8,5 +8,7 @@ ssh guard@silicon lock-get-single-server --reason $BUILD_URL > "${BUILD_NUMBER}.
 . "$(pwd)/${BUILD_NUMBER}.properties"
 
 echo "Removing old builds"
+set -f
 ssh $INTERNAL_HTTP_USER_HOST rm $(internal_novaplugins_path '*')
 ssh $INTERNAL_HTTP_USER_HOST rm $(internal_xva_path '*')
+set +f
