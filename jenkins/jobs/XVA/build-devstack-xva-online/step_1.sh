b41538f294068e21cfcbb54c901e2876d37176cc
Bob Ball <bob.ball@citrix.com>
2014-01-17 13:36:33 +0000

    Fix wildcard escaping

--- jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
+++ jenkins/jobs/XVA/build-devstack-xva-online/step_1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -eux
 
+THISDIR="$(cd "$(dirname $0)" && pwd)"
+. "${THISDIR}/../lib.sh"
+
 ssh guard@silicon lock-get-single-server --reason $BUILD_URL > "${BUILD_NUMBER}.properties"
 . "$(pwd)/${BUILD_NUMBER}.properties"
+
+echo "Removing old builds"
+ssh $INTERNAL_HTTP_USER_HOST rm $(internal_novaplugins_path '*')
+ssh $INTERNAL_HTTP_USER_HOST rm $(internal_xva_path '*')
