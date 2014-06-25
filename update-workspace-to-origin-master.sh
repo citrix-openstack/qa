bbe87b4b0dad09701b4d572506c84122752ba724
Bob Ball <bob.ball@citrix.com>
2014-06-25 12:59:34 +0100

    Add git_retry logic for when github is tempremental
    Copied from devstack's git_timed function

--- update-workspace-to-origin-master.sh
+++ update-workspace-to-origin-master.sh
@@ -6,6 +6,6 @@ echo "Creating workspace..."
 echo "Resetting workspace..."
 ./reset-workspace.sh
 echo "Fetch origin..."
-./with-all-repos-in-workspace.sh git fetch origin
+./with-all-repos-in-workspace.sh git_retry fetch origin
 echo "Check out origin/master as a new branch..."
-./with-all-repos-in-workspace.sh git checkout origin/master -B tmpbranch --no-track
+./with-all-repos-in-workspace.sh git_retry checkout origin/master -B tmpbranch --no-track
