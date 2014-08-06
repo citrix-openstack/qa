c64aa5434e3840b5f40795f07e12dbdc09259917
Bob Ball <bob.ball@citrix.com>
2014-08-06 15:45:01 +0100

    dist-upgrade was causing failures in generating the new initrd (pre-buildroot build)

--- jenkins/jobs/build-xenserver-core-ubuntu.sh
+++ jenkins/jobs/build-xenserver-core-ubuntu.sh
@@ -69,7 +69,7 @@ APT::Get::force-yes "true";
 APT_ASSUME_YES
 
 sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get upgrade
 sudo apt-get install git ocaml-nox
 
 git clone $REPO_URL xenserver-core
