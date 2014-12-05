3d7cee86cbe456aef5ce36f424faeea4cd0b1922
Mate Lakat <mlakat@mlakat.hu>
2014-12-05 11:49:32 +0100

    Refactor

--- jenkins/jobs/upload-testing-xva.sh
+++ jenkins/jobs/upload-testing-xva.sh
@@ -2,14 +2,85 @@
 set -eux
 
 
+function main() {
+    validate_parameters
+    print_out_parameters
+    check_out_infra
+    enter_infra
+    check_connection
+
+    cat > upload_script.sh << EOF
+set -eux
+rm -rf /tmp/img_upload
+EOF
+
+    if [ "$DRY_RUN" = "YES" ]; then
+        cat >> upload_script.sh << EOF
+mkdir /tmp/img_upload
+echo "hello" > /tmp/img_upload/image.xva
+EOF
+    fi
+
+    if [ "$DRY_RUN" = "NO" ]; then
+        cat >> upload_script.sh << EOF
+mv xva_images /tmp/img_upload
+EOF
+    fi
+
+    cat >> upload_script.sh << EOF
+sudo -u osci -i /opt/osci/env/bin/osci-upload \\
+    -c $CONTAINER \\
+    -r IAD /tmp/img_upload $FOLDER
+EOF
+
+    ./scp.sh prod_ci upload_script.sh upload_script.sh
+    ./ssh.sh prod_ci bash upload_script.sh
+}
+
+
+function check_out_infra() {
+    rm -rf infra
+    hg clone http://hg.uk.xensource.com/openstack/infrastructure.hg/ infra
+}
+
+
+function enter_infra() {
+    cd infra/osci
+}
+
+
+function check_connection() {
+    ./ssh.sh prod_ci echo "Connection OK"
+}
+
+
+function print_out_parameters() {
+    cat << EOF
+-------------------------------------------------------------------------------
+Uploading an XVA to rackspace cloud from prod_ci with the following parameters
+
+    DRY_RUN=$DRY_RUN
+    CONTAINER=$CONTAINER
+    FOLDER=$FOLDER
+-------------------------------------------------------------------------------
+EOF
+}
+
 function validate_parameters() {
     DRY_RUN=${DRY_RUN}
     CONTAINER=${CONTAINER}
+    FOLDER=${FOLDER}
 
     if [ -z "$CONTAINER" ]; then
         echo "CONTAINER cannot be an empty string" >&2
         exit 1
     fi
+
+    if [ -z "$FOLDER" ]; then
+        echo "FOLDER cannot be an empty string" >&2
+        exit 1
+    fi
+
 
     if [ "YES" = "$DRY_RUN" ]; then
         return;
@@ -23,27 +94,6 @@ function validate_parameters() {
     exit 1
 }
 
+main
 
-rm -rf infra
-hg clone http://hg.uk.xensource.com/openstack/infrastructure.hg/ infra
 
-cd infra/osci
-
-./ssh.sh prod_ci echo "Connection OK"
-
-if [ "$DRY_RUN" = "YES" ]; then
-    cat > upload_script.sh << EOF
-set -eux
-rm -rf /tmp/img_upload
-mkdir /tmp/img_upload
-echo "hello" > /tmp/img_upload/image.xva
-sudo -u osci -i /opt/osci/env/bin/osci-upload -c $CONTAINER -r IAD /tmp/img_upload image
-EOF
-
-    ./scp.sh prod_ci upload_script.sh upload_script.sh
-    ./ssh.sh prod_ci bash upload_script.sh
-    exit 0
-fi
-
-echo "Not implemented" >&2
-exit 1
