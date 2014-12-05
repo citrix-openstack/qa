991948c1d5c1b10759fa2ef45dbc28e9a6c8223f
Mate Lakat <mlakat@mlakat.hu>
2014-12-05 12:04:34 +0100

    Print out actual configuration

--- jenkins/jobs/set-prod-ci-link-to-image.sh
+++ jenkins/jobs/set-prod-ci-link-to-image.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -eux
 
-echo "Hello"
+PrivateKeyToPublicHttpServer="$PrivateKeyToPublicHttpServer"
+PublicHttpServerOpenStackPath="$PublicHttpServerOpenStackPath"
+PublicHttpServerUserAndHost="$PublicHttpServerUserAndHost"
+
+echo "Printing actual configuration"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    -i $PrivateKeyToPublicHttpServer \
+    $PublicHttpServerUserAndHost \
+    cat $PublicHttpServerOpenStackPath/xenapi-in-the-cloud-appliances/prod_ci
