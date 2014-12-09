bf5cd271d76573418b3696c1d79d646cc682cc62
Mate Lakat <mlakat@mlakat.hu>
2014-12-09 18:47:24 +0100

    Skeleton for neutron-in-devci.sh

---
+++ jenkins/jobs/neutron-in-devci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+THIS_FILE=$(readlink -f $0)
+THIS_DIR=$(dirname $THIS_FILE)
+
+. $THIS_DIR/infralib/functions.sh
+
+
+function main() {
+    check_out_infra
+    enter_infra
+    ls -la
+}
+
+main
