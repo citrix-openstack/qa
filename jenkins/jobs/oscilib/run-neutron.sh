fcf22ae2683fdb67f8d5c97df09e121c3cf3702a
Mate Lakat <mlakat@mlakat.hu>
2014-12-09 20:03:56 +0100

    Use local, fix finish function

--- jenkins/jobs/oscilib/run-neutron.sh
+++ jenkins/jobs/oscilib/run-neutron.sh
@@ -8,25 +8,25 @@ THIS_DIR=$(dirname $THIS_FILE)
 
 . $THIS_DIR/utils.sh
 
-NODE=""
 HELD_NODE=""
 
 
 function finish {
     if [ -n "$HELD_NODE" ]; then
         echo "Deleting held node"
-        osci-nodepool delete $(cat "$NODE" | get_node_id)
+        osci-nodepool delete $(echo "$HELD_NODE" | get_node_id)
     fi
 }
 
 
 function main() {
+    local node
     trap finish EXIT
 
     echo "Waiting for a ready node"
     while true; do
-        NODE=$(osci-nodepool list | get_ready_node)
-        if [ -z "$NODE" ]; then
+        node=$(osci-nodepool list | get_ready_node)
+        if [ -z "$node" ]; then
             echo "."
             sleep 1
             continue
@@ -35,10 +35,10 @@ function main() {
         break
     done
 
-    NODE_ID=$(echo "$NODE" | get_node_id)
-    NODE_IP=$(echo "$NODE" | get_node_ip)
+    NODE_ID=$(echo "$node" | get_node_id)
+    NODE_IP=$(echo "$node" | get_node_ip)
 
-    osci-nodepool hold "$NODE_ID" && HELD_NODE="$NODE"
+    osci-nodepool hold "$NODE_ID" && HELD_NODE="$node"
 
     cat << EOF
 Node ID: $NODE_ID
