e36a1ef99f58315ac08e2e9102e5bc06991a47e7
Mate Lakat <mlakat@mlakat.hu>
2014-12-10 13:44:23 +0100

    Kill node on exit

--- jenkins/jobs/neutron-in-devci.sh
+++ jenkins/jobs/neutron-in-devci.sh
@@ -7,12 +7,20 @@ THIS_DIR=$(dirname $THIS_FILE)
 . $THIS_DIR/infralib/functions.sh
 
 
+function finish {
+    ./ssh.sh dev_ci BUILD_ID=$BUILD_ID bash kill-node.sh
+}
+
+
 function main() {
     check_out_infra
     enter_infra_osci
     ./scp.sh dev_ci $THIS_DIR/oscilib/utils.sh utils.sh
     ./scp.sh dev_ci $THIS_DIR/oscilib/run-neutron.sh run-neutron.sh
+    ./scp.sh dev_ci $THIS_DIR/oscilib/kill-node.sh kill-node.sh
+    trap finish EXIT
     ./ssh.sh dev_ci BUILD_ID=$BUILD_ID bash run-neutron.sh
+    sleep 1000
 }
 
 
