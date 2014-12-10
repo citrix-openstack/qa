2e1e37891fab8d54408c5dc99c0d0ace1685f293
Mate Lakat <mlakat@mlakat.hu>
2014-12-10 14:04:22 +0100

    Add teardown script, fix echo/cat confusion

--- jenkins/jobs/neutron-in-devci.sh
+++ jenkins/jobs/neutron-in-devci.sh
@@ -8,21 +8,15 @@ THIS_DIR=$(dirname $THIS_FILE)
 . $THIS_DIR/oscilib/utils.sh
 
 
-function finish {
-    ./ssh.sh dev_ci BUILD_ID=$BUILD_ID bash kill-node.sh
-}
-
-
 function main() {
     check_out_infra
     enter_infra_osci
     ./scp.sh dev_ci $THIS_DIR/oscilib/utils.sh utils.sh
     ./scp.sh dev_ci $THIS_DIR/oscilib/run-neutron.sh run-neutron.sh
     ./scp.sh dev_ci $THIS_DIR/oscilib/kill-node.sh kill-node.sh
-    trap finish EXIT
     ./ssh.sh dev_ci BUILD_ID=$BUILD_ID bash run-neutron.sh
     node=$(./ssh.sh dev_ci cat ${BUILD_ID}.node)
-    node_ip=$(cat "$node" | get_node_ip)
+    node_ip=$(echo "$node" | get_node_ip)
 
     while ! ./ssh-as-jenkins.sh dev_ci $node_ip test -e /opt/stack/new/devstacklog.txt; do
         echo "Waiting for devstack log to be born"
