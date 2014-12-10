82bbc37e51e8ac3a801e763a0423c3ad342f9a23
Mate Lakat <mlakat@mlakat.hu>
2014-12-10 13:59:36 +0100

    Tail devstack log to jenkins

--- jenkins/jobs/neutron-in-devci.sh
+++ jenkins/jobs/neutron-in-devci.sh
@@ -5,6 +5,7 @@ THIS_FILE=$(readlink -f $0)
 THIS_DIR=$(dirname $THIS_FILE)
 
 . $THIS_DIR/infralib/functions.sh
+. $THIS_DIR/oscilib/utils.sh
 
 
 function finish {
@@ -20,7 +21,15 @@ function main() {
     ./scp.sh dev_ci $THIS_DIR/oscilib/kill-node.sh kill-node.sh
     trap finish EXIT
     ./ssh.sh dev_ci BUILD_ID=$BUILD_ID bash run-neutron.sh
-    sleep 1000
+    node=$(./ssh.sh dev_ci cat ${BUILD_ID}.node)
+    node_ip=$(cat "$node" | get_node_ip)
+
+    while ! ./ssh-as-jenkins.sh dev_ci $node_ip test -e /opt/stack/new/devstacklog.txt; do
+        echo "Waiting for devstack log to be born"
+        sleep 5
+    done
+
+    ./ssh-as-jenkins.sh dev_ci $node_ip tail -f /opt/stack/new/devstacklog.txt
 }
 
 
