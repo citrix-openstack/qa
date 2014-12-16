4c68a0beee6ff531ece891a299cc787a62e27825
Mate Lakat <mlakat@mlakat.hu>
2014-12-16 15:42:47 +0100

    Non-quiet mirroring

--- jenkins/jobs/mirror-repositories.sh
+++ jenkins/jobs/mirror-repositories.sh
@@ -5,8 +5,8 @@ set -eux
 sshpass -pubuntu ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@copper.eng.hq.xensource.com << EOF
 set -eux
 cd /usr/share/nginx/www
-/usr/bin/wget -m -np -nH -q "http://coltrane.eng.hq.xensource.com/release/XenServer-6.x/XS-6.1/RTM/xe-phase-1/"
-/usr/bin/wget -m -np -nH -q "http://coltrane.eng.hq.xensource.com/release/XenServer-6.x/XS-6.2/RTM-70446/xe-phase-1/"
-/usr/bin/wget -m -np -nH -q "http://coltrane.eng.hq.xensource.com/usr/groups/build/clearwater-lcm/xe-phase-3-latest/xe-phase-1/"
-/usr/bin/wget -m -np -nH -q "http://www.uk.xensource.com/linux/distros/CentOS/6.4/os/x86_64/"
+/usr/bin/wget -m -np -nH "http://coltrane.eng.hq.xensource.com/release/XenServer-6.x/XS-6.1/RTM/xe-phase-1/"
+/usr/bin/wget -m -np -nH "http://coltrane.eng.hq.xensource.com/release/XenServer-6.x/XS-6.2/RTM-70446/xe-phase-1/"
+/usr/bin/wget -m -np -nH "http://coltrane.eng.hq.xensource.com/usr/groups/build/clearwater-lcm/xe-phase-3-latest/xe-phase-1/"
+/usr/bin/wget -m -np -nH "http://www.uk.xensource.com/linux/distros/CentOS/6.4/os/x86_64/"
 EOF
