876000c16e813a87e14c491910b923d05e9f9559
John Hua <john.hua@citrix.com>
2015-08-24 16:08:39 +0800

    Remove obsolete scripts

--- fuelplugin-utils/prepare_cluster.sh
+++ fuelplugin-utils/prepare_cluster.sh
@@ -9,15 +9,13 @@ do
 	_HOST_NODE=(${HOST_NODE//// })
 	HOST=${_HOST_NODE[0]}
 	NODE=${_HOST_NODE[1]}
-	
+
 	sshpass -p $XEN_PASSWORD ssh -o StrictHostKeyChecking=no $XEN_ROOT@$HOST \
 'set +x
 guest_name="'$NODE'"
-eth0="'$NODE_ETH0'"
-eth1="'$NODE_ETH1'"
 memory="'$NODE_MEMORY'"
 disksize="'$NODE_DISKSIZE'"
-tname="Other install media"	
+tname="Other install media"
 
 vm_uuid=$(xe vm-install template="$tname" new-name-label="$guest_name")
 
@@ -36,48 +34,11 @@ xe vm-memory-limits-set \
     dynamic-max=${memory}MiB \
     uuid=$vm_uuid
 
-eth0_uuid=$(xe vif-create network-uuid=$(xe network-list name-label="$eth0" --minimal) vm-uuid=$vm_uuid device=0)
-eth1_uuid=$(xe vif-create network-uuid=$(xe network-list name-label="$eth1" --minimal) vm-uuid=$vm_uuid device=1)
-
+eth0_uuid=$(xe vif-create network-uuid=$(xe network-list name-label="'"$NODE_ETH0"'" --minimal) vm-uuid=$vm_uuid device=0)
+eth1_uuid=$(xe vif-create network-uuid=$(xe network-list name-label="'"$NODE_ETH1"'" --minimal) vm-uuid=$vm_uuid device=1)
 eth0_mac=$(xe vif-param-get uuid=$eth0_uuid param-name=MAC)
-
 xe vm-param-set uuid=$vm_uuid HVM-boot-params:order=ndc
 
-echo "Creating "'$NODE'" ($eth0_mac) on "'$HOST'""
+echo "'$HOST'	'$NODE'	($eth0_mac) booted"
 '
 done
-
-./setup_HIMN.sh
-
-echo "Booting VMs"
-for HOST_NODE in ${ALL_NODES//,/ }
-do
-	_HOST_NODE=(${HOST_NODE//// })
-	HOST=${_HOST_NODE[0]}
-	NODE=${_HOST_NODE[1]}
-	sshpass -p $XEN_PASSWORD ssh -o StrictHostKeyChecking=no $XEN_ROOT@$HOST \
-'
-guest_name="'$NODE'"
-vm_uuid=$(xe vm-list name-label="$guest_name" power-state="halted" --minimal)
-xe vm-start vm=$vm_uuid
-echo "'$NODE' booted"
-'
-done
-
-if false; then
-	sleep 60
-
-	echo "Creating Cluster"
-	sshpass -p $FUELMASTER_PASSWORD ssh -o StrictHostKeyChecking=no $FUELMASTER_ROOT@$FUELMASTER \
-'
-guest_name="'$NODE'"
-
-rel_id=$(fuel release | grep "XenServer" | cut -d " " -f1)
-fuel env create --name "'$ENV_NAME'" --rel $rel 
-env_id=$(fuel env | grep "'$ENV_NAME'" | cut -d " " -f1)
-nodes=($(fuel node | grep True | cut -d " " -f1))
-fuel --env $env_id node set --node ${nodes[0]} --role controller
-fuel --env $env_id node set --node ${nodes[1]},${nodes[2]},${nodes[3]} --role compute
-fuel --env $env_id node set --node ${nodes[4]} --role compute,cinder
-'
-fi
