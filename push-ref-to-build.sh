20d9d5d123305b5a2a184107a55382ab619e4573
Bob Ball <bob.ball@citrix.com>
2016-01-04 18:24:58 +0000

    Added some debugging

--- push-ref-to-build.sh
+++ push-ref-to-build.sh
@@ -23,17 +23,17 @@ TARGET_REF="${1-$(print_usage_and_die)}"
 SOME_REPOS="${2:-}"
 
 if [ -z "$SOME_REPOS" ]; then
-    echo "updating $TARGET_REF to point to HEAD"
-    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git update-ref "$TARGET_REF" HEAD
-    echo "Pushing HEAD as a reference $TARGET_REF to build remote"
-    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git push --quiet build HEAD:"$TARGET_REF"
-    echo "Pushing tags"
-    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git push --tags build
-else
-    echo "updating $TARGET_REF to point to HEAD"
+    echo "[All repos] updating $TARGET_REF to point to HEAD"
     ./with-all-repos-in-workspace.sh git update-ref "$TARGET_REF" HEAD
-    echo "Pushing HEAD as a reference $TARGET_REF to build remote"
+    echo "[All repos] Pushing HEAD as a reference $TARGET_REF to build remote"
     ./with-all-repos-in-workspace.sh git push --quiet build HEAD:"$TARGET_REF"
-    echo "Pushing tags"
+    echo "[All repos] Pushing tags"
     ./with-all-repos-in-workspace.sh git push --tags build
+else
+    echo "[Some repos] updating $TARGET_REF to point to HEAD"
+    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git update-ref "$TARGET_REF" HEAD
+    echo "[Some repos] Pushing HEAD as a reference $TARGET_REF to build remote"
+    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git push --quiet build HEAD:"$TARGET_REF"
+    echo "[Some repos] Pushing tags"
+    ./with-some-repos-in-workspace.sh "$SOME_REPOS" git push --tags build
 fi
--- update-workspace-to-origin-master.sh
+++ update-workspace-to-origin-master.sh
@@ -8,13 +8,13 @@ echo "Creating workspace..."
 echo "Resetting workspace..."
 ./reset-workspace.sh
 if [ -z "$SOME_REPOS" ]; then
-    echo "Fetch origin..."
+    echo "[All repos] Fetch origin..."
     ./with-all-repos-in-workspace.sh git_retry fetch origin
-    echo "Check out origin/master as a new branch..."
+    echo "[All repos] Check out origin/master as a new branch..."
     ./with-all-repos-in-workspace.sh git_retry checkout origin/master -B tmpbranch --no-track
 else
-    echo "Fetch origin..."
+    echo "[Some repos] Fetch origin..."
     ./with-some-repos-in-workspace.sh "$1" git_retry fetch origin
-    echo "Check out origin/master as a new branch..."
+    echo "[Some repos] Check out origin/master as a new branch..."
     ./with-some-repos-in-workspace.sh "$1" git_retry checkout origin/master -B tmpbranch --no-track
 fi
