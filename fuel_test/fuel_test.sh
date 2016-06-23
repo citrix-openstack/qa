dd1deb4d6d382fbfd95e6821e72b75f5664b734d
John Hua <john.hua@citrix.com>
2016-06-23 10:32:48 +0800

    Give health check more time

--- fuel_test/fuel_test.sh
+++ fuel_test/fuel_test.sh
@@ -8,5 +8,5 @@ timeout 30m ./prep_env.sh
 [ $? -ne 0 ] && echo prep_env execution timeout && exit -1
 timeout 90m ./deploy_env.sh
 [ $? -ne 0 ] && echo deploy_env execution timeout && exit -1
-timeout 15m ./test_env.sh
+timeout 30m ./test_env.sh
 [ $? -ne 0 ] && echo test_env execution timeout && exit -1
