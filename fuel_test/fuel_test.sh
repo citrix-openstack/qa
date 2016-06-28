f2e1f26d0f03b4c19889855c43233a354acce96e
John Hua <john.hua@citrix.com>
2016-06-28 10:08:21 +0800

    Increase the timeout for deployment

--- fuel_test/fuel_test.sh
+++ fuel_test/fuel_test.sh
@@ -6,7 +6,7 @@ timeout 5m ./clear_env.sh
 [ $? -ne 0 ] && echo clear_env execution timeout && exit -1
 timeout 30m ./prep_env.sh
 [ $? -ne 0 ] && echo prep_env execution timeout && exit -1
-timeout 90m ./deploy_env.sh
+timeout 120m ./deploy_env.sh
 [ $? -ne 0 ] && echo deploy_env execution timeout && exit -1
 timeout 30m ./test_env.sh
 [ $? -ne 0 ] && echo test_env execution timeout && exit -1
