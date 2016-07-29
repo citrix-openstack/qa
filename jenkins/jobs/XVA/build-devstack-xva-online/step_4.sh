3c3ffc2e77a0aeb7cb0a4ad29c3f973f102c0e7b
Huan Xie <huan.xie@citrix.com>
2016-07-29 00:04:19 -0700

    Move XVA/ISO to NFS share and only keep 6

--- jenkins/jobs/XVA/build-devstack-xva-online/step_4.sh
+++ jenkins/jobs/XVA/build-devstack-xva-online/step_4.sh
@@ -18,11 +18,22 @@ ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost \
     wget "http://10.219.13.54/builds/$(xva_name $XVA_INTERNAL_NAME)" \
     -O "$PublicHttpServerOpenStackPath/$(xva_name $XVA_NAME)" -q
 
+echo "The published build result is now located at:"
+echo "    $PublicHttpServerOpenStackLocation/$(novaplugins_name $XVA_NAME)"
+echo "    $PublicHttpServerOpenStackLocation/$(xva_name $XVA_NAME)"
+
+NFSSERVER="10.62.132.4:/openstack"
+NFSLOCAL=$(mktemp -d)
+sudo mount $NFSSERVER $NFSLOCAL
+sudo wget "http://10.219.13.54/builds/$(novaplugins_name $XVA_INTERNAL_NAME)" -O $NFSLOCAL/$(novaplugins_name $XVA_NAME) 
+sudo wget "http://10.219.13.54/builds/$(xva_name $XVA_INTERNAL_NAME)" -O $NFSLOCAL/$(xva_name $XVA_NAME)
+sudo umount $NFSLOCAL
+
+echo "The published build result is now located at:"
+echo "    $NFSSERVER/$(novaplugins_name $XVA_NAME)"
+echo "    $NFSSERVER/$(xva_name $XVA_NAME)"
+
 echo "Remove internal builds"
 ssh $INTERNAL_HTTP_USER_HOST rm -f \
     $(internal_novaplugins_path $XVA_INTERNAL_NAME) \
     $(internal_xva_path $XVA_INTERNAL_NAME)
-
-echo "The published build result is now located at:"
-echo "    $PublicHttpServerOpenStackLocation/$(novaplugins_name $XVA_NAME)"
-echo "    $PublicHttpServerOpenStackLocation/$(xva_name $XVA_NAME)"
--- jenkins/jobs/XVA/xva-remove-old-builds-from-public-server/keep_6_latest_xva_suppack.sh
+++ jenkins/jobs/XVA/xva-remove-old-builds-from-public-server/keep_6_latest_xva_suppack.sh
@@ -8,3 +8,12 @@ ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost \
 ssh -i $PrivateKeyToPublicHttpServer $PublicHttpServerUserAndHost \
     "find $PublicHttpServerOpenStackPath/ -name 'novaplugins-????_??_??.iso' | \
     sort -r | sed '1,6d' | xargs rm -vf" || true
+
+NFSSERVER="10.62.132.4:/openstack"
+NFSLOCAL=$(mktemp -d)
+sudo mount $NFSSERVER $NFSLOCAL
+find $NFSLOCAL/ -name 'devstack-????_??_??.xva' | \
+    sort -r | sed '1,6d' | xargs sudo rm -vf || true
+find $NFSLOCAL/ -name 'novaplugins-????_??_??.iso' | \
+    sort -r | sed '1,6d' | xargs sudo rm -vf || true
+sudo umount $NFSLOCAL
